55eac85c43b71405a4b28cc987196452fb2c9827
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-09-15 16:52:39 +1000

    use web socket

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+//testtest
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer ws.Close()
+
+	for {
+		err = ws.WriteMessage(websocket.TextMessage, []byte(time.Now().Format(time.ANSIC)))
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("/")))
+	http.HandleFunc("/ws", serveWs)
+
+	if err := http.ListenAndServe(":1234", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
